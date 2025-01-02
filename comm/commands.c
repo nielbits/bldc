f31b34984925a2d3eef16aef82fe9005b67f3bc4
nieltsb <nieltsb@gmail.com>
2025-01-02 15:54:05 +0100

    iq_set signal instead of amp hours

--- comm/commands.c
+++ comm/commands.c
@@ -422,7 +422,9 @@ void commands_process_packet(unsigned char *data, unsigned int len,
 			buffer_append_float16(send_buffer, mc_interface_get_input_voltage_filtered(), 1e1, &ind);
 		}
 		if (mask & ((uint32_t)1 << 9)) {
-			buffer_append_float32(send_buffer, mc_interface_get_amp_hours(false), 1e4, &ind);
+			
+			//buffer_append_float32(send_buffer, mc_interface_get_amp_hours(false), 1e4, &ind);
+			buffer_append_float32(send_buffer, mcpwm_foc_get_iq_set(), 1e4, &ind);
 		}
 		if (mask & ((uint32_t)1 << 10)) {
 			buffer_append_float32(send_buffer, mc_interface_get_amp_hours_charged(false), 1e4, &ind);
