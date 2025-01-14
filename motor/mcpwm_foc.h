f72c6847a62da6712946883466e7699b0d555206
nieltsb <nieltsb@gmail.com>
2025-01-14 16:33:57 +0100

    correction of function declaration

--- motor/mcpwm_foc.h
+++ motor/mcpwm_foc.h
@@ -149,7 +149,7 @@ void mcpwm_foc_set_current_off_delay(float delay_sec);
 void mcpwm_foc_set_gear_ratio(float gear_ratio);
 
 // Functions where the motor can be selected
-float mcpwm_foc_get_gear_ratio();
+float mcpwm_foc_get_gear_ratio(void);
 float mcpwm_foc_get_tot_current_motor(bool is_second_motor);
 float mcpwm_foc_get_tot_current_filtered_motor(bool is_second_motor);
 float mcpwm_foc_get_tot_current_in_motor(bool is_second_motor);
