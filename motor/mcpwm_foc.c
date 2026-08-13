15affb94e8290a8020bf2c08a331941d063e18e7
nielbits <nieltsb@gmail.com>
2026-08-13 11:19:48 +0200

    working

--- motor/mcpwm_foc.c
+++ motor/mcpwm_foc.c
@@ -382,9 +382,9 @@ void mcpwm_foc_init(mc_configuration *conf_m1, mc_configuration *conf_m2) {
 	m_motor_1.p_k_v_bw= 0.00001f;
 	m_motor_1.p_k_area =0.14f;//0.14
 	m_motor_1.p_height =1.75f;
-	m_motor_1.p_fo_hz= 12.0f;      // = 8.0f;          // observer bandwidth (try 10–18 Hz)//100Hz //8Hz for small motor
+	m_motor_1.p_fo_hz= 20.0f;      // = 8.0f;          // observer bandwidth (try 10–18 Hz)//100Hz //8Hz for small motor
     m_motor_1.p_gz_hz=0.2f;      // = 0.20f;           // tiny leak on z to suppress random-walk hiss (0–0.7 Hz)
-    m_motor_1.p_fc_TLPF= 30.0f; 	  // = 200.0f;   
+    m_motor_1.p_fc_TLPF= 8s0.0f; 	  // = 200.0f;   
 	m_motor_1.p_adrc_scale= 1.0f; // 
 	m_motor_1.p_speed_limit_pos_control_activation =400.0f; // Speed limit for position control activation
 	m_motor_1.leso_z4 = 0.0f;	
@@ -456,7 +456,7 @@ void mcpwm_foc_init(mc_configuration *conf_m1, mc_configuration *conf_m2) {
 	m_motor_1.gear_ratio_bike =  2.575f; //default 2.575
 
 
-	m_motor_1.p_J= 0.29f;
+	m_motor_1.p_J= 0.174f;
 	m_motor_1.p_kT= (float)(1.5f*0.01927f *23.0f);
 	m_motor_1.p_mech_gearing=(240.0f/92.2f);
 	m_motor_1.p_B= 0.052f;
